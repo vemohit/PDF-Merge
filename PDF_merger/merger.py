6706d1232dc38f25e4d48db56d7f2bd6206f312d
vemohit <42634353+vemohit@users.noreply.github.com>
2023-09-11 18:10:04 +0530

    Add files via upload

---
+++ PDF_merger/merger.py
@@ -0,0 +1,12 @@
+import PyPDF2
+import sys
+import os
+
+merger = PyPDF2.PdfMerger()
+
+for file in os.listdir(os.curdir):
+    if(file.endswith(".pdf")):
+        print(file)
+    
+        merger.append(file)
+    merger.write("combinedPDF.pdf")
